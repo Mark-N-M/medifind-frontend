170719079bce02c8c9c562c8f51fdcc31c75a9a4
Mark-N-M <marknm5757@gmail.com>
2026-08-23 12:31:29 +0300

    pharmacies page creation

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/components/HomePage.vue'
 import MedicineSearch from '@/components/MedicineSearch.vue'
+import PharmaciesPage from '../components/PharmaciesPage.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +14,10 @@ const router = createRouter({
     {
       path: '/search',
       component: MedicineSearch,
+    },
+    {
+      path: '/pharmacies',
+      component: PharmaciesPage,
     },
   ],
 })
